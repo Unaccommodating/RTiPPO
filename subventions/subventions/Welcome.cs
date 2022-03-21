5d86f9d811bd2efd04654dccea76ec580581ee6f
Rasim <email@example.com>
2022-03-21 20:32:20 +0500

    added C# project with welcome file

---
+++ subventions/subventions/Welcome.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace subventions
+{
+    public partial class Welcome : Form
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void Welcome_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Enter_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            newForm form = new newForm();
+            form.Show();
+        }
+
+        private void registration_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
