a5c023e21b068bfabf01e7250d27ccab1b35daf6
Azat <azat.abdullin72@gmail.com>
2022-04-22 14:19:51 +0500

    Котроллер и модели к 7 лабе

---
+++ subventions/subventions/Controllers/ControllerRegistry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ControllerRegistry
+{
+	public List<Subvention> Records()
+	{
+		List<Subvention> subventions = new List<Subvention>();
+		return subventions;
+	}
+	public List<Subvention> Records(int paramsSort, string paramsFilter)
+    {
+		List<Subvention> subventions = new List<Subvention>();
+		return subventions;
+	}
+	public bool checkAccess(int id)
+    {
+		return true;
+    }
+	public Subvention OpenRecord(int id)
+	{
+		Subvention subventions = new Subvention();
+		return subventions;
+	}
+	public ControllerRegistry(int id)
+	{
+		if (checkAccess(id))
+			Records();
+		else
+			return;
+	}
+	public bool exportExcel(){
+		return true;
+	}
+}
+
