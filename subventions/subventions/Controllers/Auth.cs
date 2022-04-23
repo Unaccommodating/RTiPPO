04ecd2570dc5e3cccb8ab873536cc8560aa73d36
Rasim <email@example.com>
2022-04-23 15:17:13 +0500

    7lab + dataBase

---
+++ subventions/subventions/Controllers/Auth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subventions.Controllers
+{
+    class Auth
+    {
+        public bool Enter(string login, string password)
+        {
+            if (login != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ subventions/subventions/Controllers/Registration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subventions.Controllers
+{
+    class Registration
+    {
+        public bool Enter(string login, string FIO, string password)
+        {
+            if (login != "")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
