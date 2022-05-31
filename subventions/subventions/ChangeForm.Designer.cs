88a9abb52792d552ea74a0e1dd79ae3a33020fa5
Aslan Kucherbayev <aslan.vdit15@gmail.com>
2022-06-01 01:17:19 +0500

    Revert "commit"
    This reverts commit ac9677125ae826622ba01a82c66c19de3272e0ea, reversing
    changes made to 09ec120e973c4b8ae5302ca477d0e9df20729bd1.

---
+++ subventions/subventions/ChangeForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace subventions
+{
+    partial class ChangeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChangeCloseButton = new System.Windows.Forms.Button();
+            this.ChangeButton = new System.Windows.Forms.Button();
+            this.ChangeYear = new System.Windows.Forms.TextBox();
+            this.ChangeGiver = new System.Windows.Forms.TextBox();
+            this.ChangeTaker = new System.Windows.Forms.TextBox();
+            this.ChangeMun = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChangeCloseButton
+            // 
+            this.ChangeCloseButton.Location = new System.Drawing.Point(673, 367);
+            this.ChangeCloseButton.Name = "ChangeCloseButton";
+            this.ChangeCloseButton.Size = new System.Drawing.Size(101, 42);
+            this.ChangeCloseButton.TabIndex = 40;
+            this.ChangeCloseButton.Text = "Закрыть";
+            this.ChangeCloseButton.UseVisualStyleBackColor = true;
+            this.ChangeCloseButton.Click += new System.EventHandler(this.ChangeCloseButton_Click);
+            // 
+            // ChangeButton
+            // 
+            this.ChangeButton.Location = new System.Drawing.Point(566, 367);
+            this.ChangeButton.Name = "ChangeButton";
+            this.ChangeButton.Size = new System.Drawing.Size(101, 42);
+            this.ChangeButton.TabIndex = 39;
+            this.ChangeButton.Text = "Сохранить";
+            this.ChangeButton.UseVisualStyleBackColor = true;
+            this.ChangeButton.Click += new System.EventHandler(this.ChangeButton_Click);
+            // 
+            // ChangeYear
+            // 
+            this.ChangeYear.Location = new System.Drawing.Point(372, 169);
+            this.ChangeYear.Name = "ChangeYear";
+            this.ChangeYear.Size = new System.Drawing.Size(175, 22);
+            this.ChangeYear.TabIndex = 36;
+            // 
+            // ChangeGiver
+            // 
+            this.ChangeGiver.Location = new System.Drawing.Point(372, 124);
+            this.ChangeGiver.Name = "ChangeGiver";
+            this.ChangeGiver.Size = new System.Drawing.Size(402, 22);
+            this.ChangeGiver.TabIndex = 35;
+            // 
+            // ChangeTaker
+            // 
+            this.ChangeTaker.Location = new System.Drawing.Point(372, 82);
+            this.ChangeTaker.Name = "ChangeTaker";
+            this.ChangeTaker.Size = new System.Drawing.Size(402, 22);
+            this.ChangeTaker.TabIndex = 34;
+            // 
+            // ChangeMun
+            // 
+            this.ChangeMun.Location = new System.Drawing.Point(372, 41);
+            this.ChangeMun.Name = "ChangeMun";
+            this.ChangeMun.Size = new System.Drawing.Size(402, 22);
+            this.ChangeMun.TabIndex = 33;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(28, 169);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(307, 20);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Год, на который выдана субвенция";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(92, 127);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Распределитель субвенции";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(133, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 20);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Получатель субвенции";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(81, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 20);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Муниципальноe образование";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(32, 215);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(742, 118);
+            this.dataGridView1.TabIndex = 41;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Объем выданных денежных средств";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Дата получения денежных средств";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(457, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 42);
+            this.button1.TabIndex = 42;
+            this.button1.Text = "Изменить платеж";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(348, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 42);
+            this.button2.TabIndex = 43;
+            this.button2.Text = "Добавить платеж";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 367);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 42);
+            this.button3.TabIndex = 44;
+            this.button3.Text = "Удалить платеж";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // ChangeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ChangeCloseButton);
+            this.Controls.Add(this.ChangeButton);
+            this.Controls.Add(this.ChangeYear);
+            this.Controls.Add(this.ChangeGiver);
+            this.Controls.Add(this.ChangeTaker);
+            this.Controls.Add(this.ChangeMun);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ChangeForm";
+            this.Text = "ChangeForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ChangeCloseButton;
+        private System.Windows.Forms.Button ChangeButton;
+        private System.Windows.Forms.TextBox ChangeYear;
+        private System.Windows.Forms.TextBox ChangeGiver;
+        private System.Windows.Forms.TextBox ChangeTaker;
+        private System.Windows.Forms.TextBox ChangeMun;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ subventions/subventions/Welcome.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace subventions
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enter = new System.Windows.Forms.Button();
+            this.registration = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.LoginText = new System.Windows.Forms.Label();
+            this.Psw = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enter
+            // 
+            this.enter.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.enter.Location = new System.Drawing.Point(222, 263);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(357, 40);
+            this.enter.TabIndex = 1;
+            this.enter.Text = "ВОЙТИ";
+            this.enter.UseVisualStyleBackColor = true;
+            this.enter.Click += new System.EventHandler(this.Enter_Click);
+            // 
+            // registration
+            // 
+            this.registration.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.registration.Location = new System.Drawing.Point(222, 334);
+            this.registration.Name = "registration";
+            this.registration.Size = new System.Drawing.Size(357, 40);
+            this.registration.TabIndex = 2;
+            this.registration.Text = "ЗАРЕГЕСТРИРОВАТЬСЯ";
+            this.registration.UseVisualStyleBackColor = true;
+            this.registration.Click += new System.EventHandler(this.registration_Click);
+            // 
+            // login
+            // 
+            this.login.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.login.Location = new System.Drawing.Point(222, 107);
+            this.login.Multiline = true;
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(357, 40);
+            this.login.TabIndex = 0;
+            this.login.TextChanged += new System.EventHandler(this.Login_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.password.Location = new System.Drawing.Point(222, 192);
+            this.password.Multiline = true;
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(357, 40);
+            this.password.TabIndex = 0;
+            this.password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // LoginText
+            // 
+            this.LoginText.AutoSize = true;
+            this.LoginText.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginText.Location = new System.Drawing.Point(218, 81);
+            this.LoginText.Name = "LoginText";
+            this.LoginText.Size = new System.Drawing.Size(62, 23);
+            this.LoginText.TabIndex = 3;
+            this.LoginText.Text = "Логин";
+            // 
+            // Psw
+            // 
+            this.Psw.AutoSize = true;
+            this.Psw.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Psw.Location = new System.Drawing.Point(218, 166);
+            this.Psw.Name = "Psw";
+            this.Psw.Size = new System.Drawing.Size(73, 23);
+            this.Psw.TabIndex = 4;
+            this.Psw.Text = "Пароль";
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Psw);
+            this.Controls.Add(this.LoginText);
+            this.Controls.Add(this.registration);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.login);
+            this.Name = "Welcome";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.Welcome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.Button registration;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label LoginText;
+        private System.Windows.Forms.Label Psw;
+    }
+}
+
